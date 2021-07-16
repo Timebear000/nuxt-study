8fcc9deb5b2aa92e6db053a3ef4fcdca6f16c96e
timebear000 <timebear000@gmail.com>
2021-07-16 18:08:45 +0900

    Mutaions with Vuex

---
+++ CrashCourse/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="container">
+      <Hero/>
+      <LargeCardDisplay v-for="cardInfo in largeCardInfo" :key="cardInfo.id" :cardsSection="cardInfo" />
+      <SmailCardDisplay v-for="cardInfo in smailCardInfo" :key="cardInfo.id" :cardsSection="cardInfo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import {largeCardSections,smallCardSections } from "@/assets/data.js"
+export default {
+  data(){
+    return {
+      largeCardInfo:largeCardSections,
+      smailCardInfo:smallCardSections,
+    }
+  }
+}
+</script>
+ 
+
+ <<style scoped>
+ 
+ </style>
---
+++ CrashCourse/pages/my-items.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This is my Items Pages
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'no-nav'
+}
+</script>
+
+<style scoped>
+
+</style>
